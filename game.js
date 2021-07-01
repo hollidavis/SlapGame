4c60e1f9dcd04541ddba145f613f2204a47f29a1
hollidavis <hollidavis19@gmail.com>
2021-07-01 15:23:59 -0600

    new attack function

--- game.js
+++ game.js
@@ -1,23 +1,47 @@
 let health = 100
 let hits = 0
 
+//#region NOTE Phase 1
+// function slap() {
 
-function slap() {
-  health--
-  hits++
-  update()
-}
-function punch() {
-  health -= 5
-  hits++
-  update()
-}
-function kick() {
-  health -= 10
-  hits++
-  update()
+//   health--
+//   hits++
+//   update()
+// }
+// function punch() {
+//   health -= 5
+//   hits++
+//   update()
+// }
+// function kick() {
+//   health -= 10
+//   hits++
+//   update()
+// }
+//#endregion
+
+let attacks = {
+  slap: {
+    damage: 1
+  },
+
+  punch: {
+    damage: 5
+  },
+  kick: {
+    damage: 10
+  }
 }
 
+function pain(attackName) {
+  let type = attacks[attackName]
+  if (health > 0) {
+    health -= type.damage
+  } else {
+    health = 0
+  }
+  update()
+}
 
 function update() {
   document.getElementById('health').innerText = health
