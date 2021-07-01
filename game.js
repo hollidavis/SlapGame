bf7d0290608ae7dcbec2d7c5dc5bf6ead53c3d52
hollidavis <hollidavis19@gmail.com>
2021-07-01 16:28:47 -0600

    Rocky styling

--- game.js
+++ game.js
@@ -35,18 +35,29 @@ let attacks = {
 
 function pain(attackName) {
   let type = attacks[attackName]
-  if (health > 0) {
-    health -= type.damage
-  } else {
+  health -= type.damage
+  if (health < 0) {
     health = 0
+    alert('they have died')
+    document.getElementById('slap-button').disabled = true
+    document.getElementById('punch-button').disabled = true
+    document.getElementById('kick-button').disabled = true
   }
   update()
 }
 
-function update() {
-  document.getElementById('health').innerText = health
-  document.getElementById('hits').innerText = hits
+function reset() {
+  document.getElementById('slap-button').disabled = false
+  document.getElementById('punch-button').disabled = false
+  document.getElementById('kick-button').disabled = false
+  health = 100
+  hits = 0
+  update()
+}
 
+function update() {
+  document.getElementById('health').innerText = health.toString()
+  document.getElementById('hits').innerText = hits.toString()
 }
 
 update()
