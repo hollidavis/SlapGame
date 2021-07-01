ddf0b4ab8f57f91550fe4de8a69855ab5ad6f3e6
hollidavis <hollidavis19@gmail.com>
2021-07-01 14:56:02 -0600

    inital v1

---
+++ game.js
@@ -0,0 +1,29 @@
+let health = 100
+let hits = 0
+
+
+function slap() {
+  health--
+  hits++
+  update()
+}
+function punch() {
+  health -= 5
+  hits++
+  update()
+}
+function kick() {
+  health -= 10
+  hits++
+  update()
+}
+
+
+function update() {
+  document.getElementById('health').innerText = health
+  document.getElementById('hits').innerText = hits
+
+}
+
+update()
+
